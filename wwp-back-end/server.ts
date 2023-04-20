d7940a5d63e7db39e39272ae0aa004f1a3277015
leonpraktyki <m.brzozowski@artneo.pl>
2023-04-20 15:51:03 +0200

    main server file and prisma schema

--- wwp-back-end/server.ts
+++ wwp-back-end/server.ts
@@ -1,6 +1,26 @@
-import Koa from "koa";
+import bodyParser from "koa-bodyparser";
 import Router from "koa-router";
+import Koa from "koa";
 
-import json from "koa-json";
+import { PrismaClient } from "@prisma/client";
 
 const app = new Koa();
+const router = new Router();
+const prisma = new PrismaClient();
+
+router.get("/find", async (ctx, next) => {
+  console.log(ctx.request.body);
+
+  const user = await prisma.planet_osm_point.findFirst({ skip: 3 });
+
+  ctx.body = user;
+  await next();
+});
+
+app.use(bodyParser());
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(3000, async () => {
+  await prisma.$connect();
+  console.log("wwp-backend: listening on http://localhost:3000");
+});
